37ba6728e3dbc31b66caf7376ec43312af49d0e7
Improve Your Skills <230791745+illegalement@users.noreply.github.com>
2025-11-26 17:28:09 -0300

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -75,7 +75,7 @@ while ($true) {
         $ChoserPF = Read-Host "Enter the option number."
 
         if ($ChoserPF -eq 1) {
-                       Write-Host "Downloading Hitdelay..."
+                       Write-Host "Downloading Hitdelay..." -ForegroundColor Cyan
             $urlPB = "https://download850.mediafire.com/1itxwl8y65gg_lRoRHbljNjD1sxqS-FWkhb_xy_oT2sdP7ClnPXOTlY-IlcxrHdrDmiFyi1M3gJeH_b8dokTBI6wW2JMb8GngH4hwbXiYaBrjoaqbJJ99LDb7j_KBHQL4qgvE1pxSjioL6d6Qgo3r0RX8qk_9C4vDt5C4MnyUVpvqOrh/nrfn5j8f6qqcwse/Lunar+%28Use+Both%29.zip"
             $destinationFilePB = "$OutfilePath\Lunar (Use Both).zip"
             Invoke-WebRequest -Uri $urlPB -OutFile $destinationFilePB
@@ -84,7 +84,7 @@ while ($true) {
             Clear-Host
         }
         elseif ($ChoserPF -eq 2) {
-                       Write-Host "Downloading Hitdelay..."
+                       Write-Host "Downloading Hitdelay..." -ForegroundColor Cyan
             $urlPB = "https://download1980.mediafire.com/mw0i1c4e1lygCBIXhg1QNjtSxnVo1GshAsQETkRjloO6k5UEnKSPE_0VFD0En6OBBV6O_RoqQGDnlE0gArIm5rJak3_3OOikzutA97AgCF67umlrHIV23iMISgHJSbKsX_cnSaXW5V45MjzHzll0tu8dBMr5bHd-8Wz66BMvPz1-xs15/dpm01zgpudtho6r/Badlion.zip"
             $destinationFilePB = "$OutfilePath\Badlion.zip"
             Invoke-WebRequest -Uri $urlPB -OutFile $destinationFilePB
@@ -93,7 +93,7 @@ while ($true) {
             Clear-Host
         }
         elseif ($ChoserPF -eq 3) {
-                       Write-Host "Downloading Hitdelay..."
+                       Write-Host "Downloading Hitdelay..." -ForegroundColor Cyan
             $urlPB = "https://download1581.mediafire.com/f0md1ya6jbsgLb3T51C1TU4vTxpC8EKSsXZTst5WV7xBK_5jGh9VOe5BE-6TKu6Nzfj34gbzRiorORcGCXCzQ-4HQX-Cc_pMVoobxskrSdrdPnvcLXoUw1PVDUltaAGFBs1qDT6y7dCMXT-mCANCCrbtYp-hCLeqOv5J2TpeTedCSnNJ/2jxrp9v8bxv7dxl/Forge.zip"
             $destinationFilePB = "$OutfilePath\Forge.zip"
             Invoke-WebRequest -Uri $urlPB -OutFile $destinationFilePB
@@ -116,7 +116,7 @@ while ($true) {
         $Choser2 = Read-Host "Enter the option number."
 
         if ($Choser2 -eq 1) {
-                       Write-Host "Downloading Hitdelay..."
+                       Write-Host "Downloading Lunar QT..." -ForegroundColor Cyan
             $urlPB = "https://download1347.mediafire.com/pdux5nqynf3gj9TkNmFf3NUUI8aSjMibO8XhrToSjdoouLq8mulKSMnwm66o5GFRTyrN_bNSgpw9XWryR1PvUyImL2ZTaEOjKjDegoR_XAG52wm0-ArTHdC0Lm5_h_BjmfBnJqaDf80tdMQovApAFp_pY6rCclHdbw8mDt02Ti7exz6W/g9o1wosjlkavrvh/LunarQT.zip"
             $destinationFilePB = "$OutfilePath\LunarQT.zip"
             Invoke-WebRequest -Uri $urlPB -OutFile $destinationFilePB
